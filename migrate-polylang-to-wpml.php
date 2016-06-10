054a00aa8a720ebb1c74b25ffa439d006341de0d
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2016-06-10 12:55:23 +0200

    Fixed taxonomy import

--- migrate-polylang-to-wpml.php
+++ migrate-polylang-to-wpml.php
@@ -11,6 +11,8 @@ class Migrate_Polylang_To_WPML {
 		$this->polylang_option = get_option('polylang');
 		
 		add_action('admin_menu', array($this, 'admin_menu'));
+		
+		add_action('admin_init', array($this, 'handle_migration'));
 	}
 	
 	public function admin_menu() {
@@ -23,24 +25,31 @@ class Migrate_Polylang_To_WPML {
 	public function migrate_page() {
 		?>
 <div class="wrap">
-	<h2>Migrate data from Polylang to WPML</h2>
-	<?php 
-	if (isset($_POST['migrate_wpml_action']) && $_POST['migrate_wpml_action'] == 'migrate') {
-		$this->migrate();
-		?>
-<div class="notice notice-success is-dismissible">
-Migration done			
-</div>
-		<?php
-	}
-	?>
-	
+	<h2><?php _e('Migrate data from Polylang to WPML', 'migrate-polylang'); ?></h2>
 	<form method="post" action="tools.php?page=polylang-importer">
 		<input type="hidden" name="migrate_wpml_action" value="migrate" />
-		<input type="submit" name="migrate-polylang-wpml" value="Migrate">
+		<input type="submit" 
+			   name="migrate-polylang-wpml" 
+			   value="<?php _e('Migrate', 'migrate-polylang'); ?>" 
+			   class="button button-primary" >
 		
 	</form>
 </div>	
+		<?php
+	}
+	
+	public function handle_migration() {
+		if (isset($_POST['migrate_wpml_action']) && $_POST['migrate_wpml_action'] == 'migrate') {
+			$this->migrate();
+			add_action( 'admin_notices', array($this, 'migrate_page_done_notice') );
+		}
+	}
+	
+	public function migrate_page_done_notice() {
+		?>
+		<div class="notice notice-success is-dismissible">
+		<?php _e('Migration done. Now you can disable this plugin forever.', 'migrate-polylang'); ?>
+		</div>
 		<?php
 	}
 	
@@ -124,8 +133,14 @@ Migration done
 	}
 	
 	private function migrate_taxonomies() {
+		global $wpdb;
 		
 		register_taxonomy('term_translations', null);
+		
+		// remove just registered taxonomy from icl_translations
+		$table = $wpdb->prefix . "icl_translations";
+		
+		$wpdb->delete($table, array('element_type' => 'tax_term_translations'));
 		
 		$pll_term_translations = get_terms('term_translations', array( 'hide_empty' => false));
 		
@@ -135,42 +150,96 @@ Migration done
 			foreach ($pll_term_translations as $pll_term_translation) {
 				$relation = maybe_unserialize( $pll_term_translation->description );
 				
-				$original_term_id = $relation[$default_language];
+				if (isset($relation[$default_language])) {
+					$original_term_id = $relation[$default_language];
 				
-				$original_term = get_term($original_term_id);
-				
-				$taxonomy = $original_term->taxonomy;
-				
-				$taxonomy = apply_filters( 'wpml_element_type', $taxonomy );
-				
-				do_action('wpml_set_element_language_details', array(
-					'element_id' => $original_term_id,
-					'element_type' => $taxonomy,
-					'trid' => false,
-					'language_code' => $default_language
-				));
-				
-				$original_term_language_details = apply_filters('wpml_element_language_details', null, array(
-					'element_id' => $original_term_id,
-					'element_type' => $taxonomy
-				));
-				
-				$trid = $original_term_language_details->trid;
-				
-				unset($relation[$default_language]);
-				
-				foreach ($relation as $language_code => $term_id) {
-					do_action('wpml_set_element_language_details', array(
-						'element_id' => $term_id,
-						'element_type' => $taxonomy,
-						'trid' => $trid,
-						'language_code' => $language_code,
-						'source_language_code' => $default_language
-					));
+					$original_term = $this->get_term_by_term_id( $original_term_id );
+					
+					$original_term_taxonomy_id = $original_term->term_taxonomy_id;
+					
+					if (isset($original_term->taxonomy)) {
+						$taxonomy = $original_term->taxonomy;
+
+						$taxonomy = apply_filters( 'wpml_element_type', $taxonomy );
+
+						try {
+							do_action('wpml_set_element_language_details', array(
+								'element_id' => $original_term_taxonomy_id,
+								'element_type' => $taxonomy,
+								'trid' => false,
+								'language_code' => $default_language
+							));
+						} catch (Exception $e) {
+							echo $e->getMessage();
+							echo "<br>Setting original term language details failed<br>";
+							printf('element_id was %s, element_type was %s, language_code was %s', 
+									$original_term_taxonomy_id, $taxonomy, $default_language);
+							exit();
+						}
+						
+
+						$original_term_language_details = apply_filters('wpml_element_language_details', null, array(
+							'element_id' => $original_term_taxonomy_id,
+							'element_type' => $taxonomy
+						));
+
+						$trid = $original_term_language_details->trid;
+
+						unset($relation[$default_language]);
+
+						foreach ($relation as $language_code => $term_id) {
+							$translated_term = $this->get_term_by_term_id( $term_id );
+							
+							$translated_term_taxonomy_id = $translated_term->term_taxonomy_id;
+							
+							try {
+								do_action('wpml_set_element_language_details', array(
+									'element_id' => $translated_term_taxonomy_id,
+									'element_type' => $taxonomy,
+									'trid' => $trid,
+									'language_code' => $language_code,
+									'source_language_code' => $default_language
+								));
+							} catch (Exception $e) {
+								echo $e->getMessage();
+								echo "<br>Setting translated term language details failed<br>";
+								printf('element_id was %s, element_type was %s, trid %s, language_code was %s, original language %s', 
+										$translated_term_taxonomy_id, $taxonomy, $trid, $language_code, $default_language);
+								exit();
+							}
+						}
+					}
+
 				}
 				
 			}
 		}
+	}
+	
+	private function get_term_by_term_id($id) {
+		global $wpdb;
+		
+		$table_name = $wpdb->prefix . "term_taxonomy";
+		
+		$select_statement = "SELECT * FROM $table_name WHERE term_id = %d";
+		
+		$select_sanitized = $wpdb->prepare($select_statement, $id);
+		
+		$term_taxonomy = $wpdb->get_row($select_sanitized);
+		
+		$term = false;
+		
+		if (isset($term_taxonomy->term_taxonomy_id) && isset($term_taxonomy->taxonomy)) {
+			$term = get_term_by('id', $id, $term_taxonomy->taxonomy);
+		}
+		
+		return $term;
+		
+	}
+	
+	private function migrate_strings() {
+		// case 'icl_st_save_translation' --> function icl_add_string_translation
+		// 
 	}
 }
 
