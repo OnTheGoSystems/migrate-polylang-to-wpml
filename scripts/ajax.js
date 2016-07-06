7c3d8e939ec6f08e79c73c13987777e814a6b228
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2016-07-06 13:00:12 +0200

    comp-1918 MPW - after migration additional option to drop Polylang data
    done

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -27,11 +27,29 @@ jQuery(document).ready(function( $ ) {
 																	$('#mpw_ajax_result').append("<div>"+resp.data.msg+"</div>");
 																	$('#mpw_ajax_result').append("<div><strong>"+mpw_ajax_str.mig_done+"</strong></div>");
 																
-																	$('#migrate_polylang_wpml').prop('disabled', false);
+																	$('#migrate_polylang_wpml').prop('disabled', false).val(mpw_ajax_str.mig_again_label);
+																	
+																	$('#remove_polylang_data').show();
 																});
 													});
 										});
 							});
 				});
+	});
+	
+	$('#remove_polylang_data').click(function(event) {
+		event.preventDefault();
+		if (confirm(mpw_ajax_str.delete_confirm)) {
+			$('#remove_polylang_data').prop('disabled', true);
+			
+			$('#remove_polylang_data_result').empty().append("<div>"+mpw_ajax_str.del_start+"</div>");
+			
+			$.post(ajaxurl,{'action':'mpw_delete_polylang_data'})
+				.done(function(resp){
+					$('#remove_polylang_data_result').append("<div>"+resp.data.msg+"</div>");
+					$('#remove_polylang_data').hide();
+					$('#migrate_polylang_wpml').hide();
+				});
+		}
 	});
 });
