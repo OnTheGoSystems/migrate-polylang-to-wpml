ea50e7660d070c4210ea9c0d2de2dd4caf73f4da
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-02-17 12:38:25 +0100

    wpmlbridge-206 migrate-polylang-to-wpml: importing terms doesn't terminate
    - added checking if term retrivied form database exists
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlbridge-206

--- classes/class-mpw_polylang_data.php
+++ classes/class-mpw_polylang_data.php
@@ -4,78 +4,78 @@
  * @author konrad
  */
 class mpw_polylang_data {
-	
+
 	private static $terms;
-	
+
 	public function __construct() {
-		
+
 	}
-	
+
 	public function get_languages() {
 		return $this->get_terms('language');
 	}
-	
+
 	public function get_term_languages() {
 		return $this->get_terms('term_language');
 	}
-	
+
 	public function get_post_translations() {
 		return $this->get_terms('post_translations');
 	}
-	
+
 	public function get_term_translations() {
 		return $this->get_terms('term_translations');
 	}
-	
+
 	private function get_terms($tax) {
-		
+
 		if (!isset(self::$terms[$tax])) {
 			global $wpdb;
-		
+
 			register_taxonomy($tax, null);
 			$table = $wpdb->prefix . "icl_translations";
 			$wpdb->delete($table, array('element_type' => 'tax_'.$tax));
 			$terms = get_terms($tax, array( 'hide_empty' => false));
-			
+
 			self::$terms[$tax] = $terms;
 		}
-				
+
 		return self::$terms[$tax];
 	}
-	
+
 	public function get_additional_languages_names() {
 		$pll_languages = $this->get_languages();
 		$default_language_slug = $this->get_default_language_slug();
-		
+
 		$additional_languages = array();
-		
+
 		foreach ($pll_languages as $language) {
 			if ($language->slug !== $default_language_slug) {
 				$additional_languages[] = $language->name;
 			}
 		}
-		
+
 		return $additional_languages;
 	}
-	
+
 	public function get_default_language_slug() {
 		$polylang_option = get_option('polylang');
-		
+
 		return $polylang_option['default_lang'];
 	}
-	
+
 	public function get_default_language_name() {
 		$pll_languages = $this->get_languages();
 		$default_language_slug = $this->get_default_language_slug();
-		
+
 		foreach ($pll_languages as $language) {
 			if ($language->slug == $default_language_slug) {
 				return $language->name;
 			}
 		}
-		
+
 	}
-	
+
 	public function get_languages_map() {
 		$polylang_languages = $this->get_languages();
 
@@ -87,7 +87,7 @@ class mpw_polylang_data {
 
 		return $polylang_languages_map;
 	}
-	
+
 	public function lang_slug_to_wpml_format($slug) {
 
 		$different = array(
@@ -108,54 +108,54 @@ class mpw_polylang_data {
 		if (isset($different[$slug])) {
 			$slug = $different[$slug];
 		}
-		
+
 		return $slug;
 	}
-	
-	
+
+
 	public function delete_data() {
 		update_option('mpw_polylang_data_deleted', 1);
 		$this->delete_options();
 		$this->delete_posts();
 		$this->delete_taxonomies();
 	}
-	
+
 	private function delete_options() {
 		delete_option('polylang');
 		delete_option('polylang_wpml_strings');
 		delete_option('polylang_widget');
 	}
-	
+
 	private function delete_posts() {
 		$posts = get_posts(array(
 			'posts_per_page' => -1,
-			'post_type' => 'polylang_mo', 
+			'post_type' => 'polylang_mo',
 			'post_status' => 'any'
 		));
-		
+
 		if ($posts) {
 			foreach ($posts as $post) {
 				wp_delete_post($post->ID, true);
 			}
 		}
 	}
-	
+
 	private function delete_taxonomies() {
 		$this->delete_tax('language');
 		$this->delete_tax('term_language');
 		$this->delete_tax('post_translations');
 		$this->delete_tax('term_translations');
 	}
-	
+
 	private function delete_tax($tax) {
 		$method_name = "get_";
 		$method_name .= $tax;
 		if ($tax == "language" || $tax == "term_language") {
 			$method_name .= "s";
 		}
-		
+
 		$terms = $this->{$method_name}();
-		
+
 		if ($terms && !empty($terms)) {
 			foreach ($terms as $term) {
 				wp_delete_term($term->term_id, $tax);
