5986a7274163247523884515795ff889a20b00b5
David Garcia Watkins <david.gw@onthegosystems.com>
2026-08-24 13:38:03 +0200

    [m2wpml-24] Simplify PHPStan API declarations

--- tests/phpstan/sitepress-stub.php
+++ tests/phpstan/sitepress-stub.php
@@ -5,20 +5,17 @@
  * @package MigratePolylangToWPML
  */
 
-if ( ! class_exists( 'SitePress' ) ) {
-	/**
-	 * Partial SitePress definition used by static analysis.
-	 */
-	class SitePress {
+/**
+ * Partial SitePress definition used by static analysis.
+ */
+class SitePress {
 
-		/**
-		 * Creates a translated duplicate of a post.
-		 *
-		 * @param int    $original_post_id Original post ID.
-		 * @param string $language_code    Target language code.
-		 */
-		public function make_duplicate( $original_post_id, $language_code ) {
-			unset( $original_post_id, $language_code );
-		}
+	/**
+	 * Creates a translated duplicate of a post.
+	 *
+	 * @param int    $original_post_id Original post ID.
+	 * @param string $language_code    Target language code.
+	 */
+	public function make_duplicate( $original_post_id, $language_code ) {
 	}
 }
